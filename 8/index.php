49936fce02e709f15e995a604e7556c19c0e8080
TakayukiKubo <herr.sutaka@gmail.com>
2015-09-27 11:23:36 +0900

    news.php実装、new_list.phpは未だ実装出来ていません。 ニュースアプリテーブル定義書、及びSQLファイルをコミット

--- 8/index.php
+++ 8/index.php
@@ -1,36 +1,56 @@
 <?php 
 //newsに表示用の変数を定義
-$view ="";
-//DB接続
+//データベースから取得したカテゴリーと名前を格納する変数
+$category_lists =array();
+
+//カテゴリー毎にhtml表示する変数
+$category_news_block = "";
+
+//カテゴリーテーブルかｒカテゴリー一覧を、取り敢えずカテゴリーIDの小さい順から取得
 $pdo = new PDO("mysql:host=localhost;dbname=news_app;charset=utf8", "root", "");
-//create_dateの降順に3件取得
-//日付の取得をMySQLの関数DATE_FORMATで整形
-//categoryテーブルを作りそこからカテゴリー情報を取得
-$sql="SELECT news.news_id, category.category_name, news.news_title, DATE_FORMAT(news.create_date, '%Y.%m.%d') AS create_date FROM news,category WHERE news.category_id = category.category_id ORDER BY news.create_date DESC LIMIT 3";
-//var_dump($sql);
+$sql="SELECT * FROM category ORDER BY category_id";
 $stmt=$pdo->prepare($sql);
 $stmt->execute();
-$results=$stmt->fetchAll(PDO::FETCH_ASSOC);
-$pdo=null;
+$category_lists=$stmt->fetchAll(PDO::FETCH_ASSOC);
 
-foreach($results as $row){
-    //var_dump($row);
-    $title = $row["news_title"];
-    $title = mb_substr($title,0,20,"utf-8");
-    $title = '<a href="news.php?news_id=' . $row["news_id"] . '">' . $title . '</a>';
-    $view .= '<dt>' . $row["create_date"] . '</dt>';
-    $view .= '<dd>' . $title . '</dd>';
-    $view .= '<dd>' . $row["category_name"] . '</dd>';
+//var_dump($category_lists);
+//各カテゴリー毎に、for文でブロックを作る
+foreach($category_lists as $category_row){
+    //var_dump($category_row);
+    $news_list = array();
+    $category_id = $category_row["category_id"];
+    $category_name = $category_row["category_name"];
+    $category_news_block .= '<h2><a href ="news_list.php?category_id=' . $category_row["category_id"].'">'.$category_name.'</a></h2>';
+//カテゴリー毎に、ニューステーブルから3件ずつニュースを取得するSQLを発行する
+    $sql = "SELECT * FROM news WHERE category_id = :category_id ORDER BY create_date DESC LIMIT 3";
+    $stmt = $pdo->prepare($sql);
+    $stmt ->bindValue(":category_id",$category_id, PDO::PARAM_INT);
+$stmt ->execute();
+$news_list = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//var_dump($news_list);
+
+$category_news_block .= "<ul>";
+foreach($news_list as $news){
+//各ニュースのタイトルを表示する
+    $news_title = $news["news_title"];
+    $category_news_block .= '<li><a  href = "news.php?news_id=' . $news["news_id"].'">' . $news_title . '</li>';
 }
-
+$category_news_block .= "</ul>";
+}
+$pdo = null;
 ?>
 
 <?php include("header.php"); ?>
-<h1>NEWS</h1>
+
+<!--PHPはここで出力開始-->
+<?php echo $category_news_block ?>
+<!--PHPはここで出力終了-->
+
+
+<!--<h1>NEWS</h1>
 <h2>テクノロジー</h2>
 <ul>
 <dl>
-    <?php echo $view ?>
 </dl>
 <li><p><a href="news_list.php">もっと見る</a></p></li>
 </ul>
@@ -54,5 +74,5 @@ foreach($results as $row){
 <li><p><a href="news.php">ニュース2</a></p></li>
 <li><p><a href="news.php">ニュース3</a></p></li>
 <li><p><a href="news_list.php">もっと見る</a></p></li>
-</ul>
+</ul>-->
 <?php include("footer.php"); ?>
--- 8/news_list.php
+++ 8/news_list.php
@@ -1,3 +1,29 @@
+<?php
+$category_id = $_GET["category_id"];
+$category_name;
+$news_title;
+$news_detail;
+$author;
+//var_dump($category_id);
+$pdo= new PDO("mysql:host=localhost;dbname=news_app;charset=utf8", "root", "");
+$sql="SELECT * FROM news WHERE category_id = $category_id";
+//var_dump($sql);
+$stmt= $pdo->prepare($sql);
+$stmt->execute();
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//var_dump($results);
+foreach($results as $news_row){
+$news_title = $news_row["news_title"];
+$news_detail = $news_row["news_detail"];
+    if(mb_strlen($news_detail) > 14){
+        $news_summary = mb_substr($news_detail,0,14);
+    }
+//var_dump($news_summary);
+
+$author = $news_row["author"];
+}
+$pdo = null;
+?>
 <?php include("header.php") ?>
 <h1><a href="index.php">NEWS</a></h1>
 <h2>速報</h2>
